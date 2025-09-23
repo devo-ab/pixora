8dc6c515eba1923caa60a19b5ced37bfb000a259
Avijit Biswas <per.useravi0@gmail.com>
2025-09-23 23:44:55 +0600

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { useReducer } from "react";
+import Glow from "./components/Glow";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import { initialState, reducer } from "./reducer/AiReducer";
+import { AiContext } from "./context/AiContext";
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <>
-      <p>Hello! Pixora</p>
-    </>
+    <div className="text-white">
+      <div className="container mx-auto px-4 py-8 max-w-6xl">
+        <AiContext.Provider value={{ state, dispatch }}>
+          <Header></Header>
+          <Glow></Glow>
+          <Main></Main>
+        </AiContext.Provider>
+      </div>
+    </div>
   );
 }
 

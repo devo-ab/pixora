1c251d35652389f841d69dd0cba17c0256f26622
Avijit Biswas <per.useravi0@gmail.com>
2025-09-28 12:55:36 +0600

    added create image page

---
+++ src/Pages/CreateImages.jsx
@@ -0,0 +1,17 @@
+import ImageSection from "../components/ImageSection";
+import Search from "../components/Search";
+import Settings from "../components/Settings";
+
+export default function CreateImages() {
+  return (
+    <div className="relative z-10">
+      <h2 className="text-4xl font-bold mb-8">
+        Let's create a masterpiece, Alvian! <span className="text-2xl">👋</span>
+      </h2>
+
+      <Settings></Settings>
+
+      <ImageSection></ImageSection>
+    </div>
+  );
+}

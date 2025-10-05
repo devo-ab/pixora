5ddf8c1a57a8da19c5da3f5d7fb8bca5ee12a485
Avijit Biswas <per.useravi0@gmail.com>
2025-10-05 22:01:23 +0600

    added reducer

---
+++ src/reducer/AiReducer.js
@@ -0,0 +1,56 @@
+const initialState = {
+  selectedTab: "createImage",
+  images: [],
+  downloads: [],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "tab":
+      return { ...state, selectedTab: action.payload };
+
+    // case "generate_image":
+    //   return {
+    //     ...state,
+    //     images: action.payload.map((url) => ({
+    //       url,
+    //       loading: true,
+    //       error: false,
+    //     })),
+    //   };
+    //     return {
+    //   ...state,
+    //   images: action.payload,
+    // };
+
+    // case "image_loaded":
+    //   return {
+    //     ...state,
+    //     images: state.images.map((img, index) =>
+    //       index === action.payload ? { ...img, loading: false } : img
+    //     ),
+    //   };
+
+    // case "image_error":
+    //   return {
+    //     ...state,
+    //     images: state.images.map((img, index) =>
+    //       index === action.payload ? { ...img, loading: false, error: true } : img
+    //     ),
+    //   };
+
+  //   case "download_image":
+  //     if (state.downloads.includes(action.payload)) {
+  //       return state;
+  //     }
+  //     return {
+  //       ...state,
+  //       downloads: [...state.downloads, action.payload],
+  //     };
+
+  //   default:
+  //     return state;
+  // }
+}
+
+export { initialState, reducer };
